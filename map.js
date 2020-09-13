4646fc6880c518857cc21f49a1ae938943a6b044
Preethi <preethi.merugumala@gmail.com>
2020-09-12 18:34:18 -0700

    Trying geolocation with mozilla API

--- map.js
+++ map.js
@@ -1,18 +1,31 @@
-var button = jQuery('.button');
-var preloader = jQuery('#preloader');
-var longitudediv = jQuery('.longitude');
-var lattitudediv = jQuery('.lattitude');
-var locationdiv = jQuery('.location');
+function geoFindMe() {
 
-if (navigator.geolocation) {    
-    // Browser supports it, we're good to go!     
-    } else {    
-    alert('Sorry your browser doesn\'t support the Geolocation API');    
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     }
-
-    button.click(function(e) {
-        e.preventDefault();
-        preloader.show();
-        navigator.geolocation.getCurrentPosition(exportPosition, errorPosition);
-    });
-
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
