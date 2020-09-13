b3a3c8a75ce1979b762f16349c49a7fb0741eb5d
Rachel Phuong <rphuong@cs.washington.edu>
2020-09-12 20:16:33 -0700

    Editing form submission

--- form.js
+++ form.js
@@ -1,10 +1,13 @@
 (function() {
-  async function submitForm(event) {
-    event.preventDefault()
-    location = document.getElementById('where')
-    duration = document.getElementById('when')
-    description = document.getElementById('desc')
-    name = "bob"
+  function submitForm() {
+//    location = document.getElementById('where')
+//    duration = document.getElementById('when')
+//    description = document.getElementById('desc')
+    sendData().then()
+  }
+
+  async function sendData() {
+  name = "bob"
     phone = "123"
     lat = 14
     long = 20
@@ -14,11 +17,12 @@
     var url = "http://localhost:5000/request?".concat(
       "name=", name, "&phone=", phone, "&lat=", lat, "&long=", long,
       "&text=", text, "&duration=", dur)
-    fetch(url).then()
-  }
+    var response = await fetch(url)
+    if (!response.ok) {
+        console.log("wrong")
+    }
+    }
 
-  document.getElementById('form').addEventListener('submit', submitForm)
-
-
+  document.getElementById('submit').addEventListener('click', submitForm)
 
 })()
